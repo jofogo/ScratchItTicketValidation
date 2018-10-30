f69c36d0ea694b8f830e797217528641b0539cf1
John Forrest Go <go_jfd@yahoo.com>
2018-10-30 21:38:36 +0800

    Added new fragments: settings, validation, scanner

--- app/src/main/java/sitv/combiz/com/scratchitticketvalidation/MainActivity.java
+++ app/src/main/java/sitv/combiz/com/scratchitticketvalidation/MainActivity.java
@@ -4,12 +4,21 @@ import android.app.Activity;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_loggedout, menu);
+        return true;
     }
 }
